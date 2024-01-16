9602b64233c9b8898872c5208de6449d82d8e413
Carla Marina Apata <carla.apata@gmail.com>
2024-01-16 17:12:53 -0300

    first commit

--- src/productMaganer.js
+++ src/productMaganer.js
@@ -1,114 +1,130 @@
 /* --------------CAPA DE NEGOCIO---------------- */
 
-const { error } = require('console');
-
-const fs = require('fs').promises;
+const fs = require("fs").promises;
 class ProductManager {
   constructor(ruta) {
-    this.path = ruta,
-    this.#id = 0
+    (this.path = ruta), (this.#id = 0);
   }
 
   #id;
 
-  getProducts = async () =>  {
+  getProducts = async () => {
     try {
-      let colectionJSON = await fs.readFile(this.path, 'utf-8')
-      return JSON.parse(colectionJSON)
-    } 
-    catch (error) {
-      await fs.writeFile(this.path, '[]');
-      let colectionJSON = await fs.readFile(this.path, 'utf-8')
-      return JSON.parse(colectionJSON)
+      let colectionJSON = await fs.readFile(this.path, "utf-8");
+      return JSON.parse(colectionJSON);
+    } catch (error) {
+      await fs.writeFile(this.path, "[]");
+      let colectionJSON = await fs.readFile(this.path, "utf-8");
+      return JSON.parse(colectionJSON);
     }
-  } 
+  };
 
-  addProduct =  async (product) => {
+  addProduct = async ({
+    id = null,
+    title,
+    description,
+    price,
+    thumbnails = [],
+    code,
+    stock,
+    category,
+    status = true,
+  }) => {
     try {
-      let {title, description, price, thumbnails, code, stock, category,id} = product
-      if (!title || !description || !price || !code || !stock || !category) {
-        console.log(`El producto debe tener todos los campos completos`);
-      }
       let colectionJSON = await this.getProducts();
-      let exists = colectionJSON.some((prod) => prod.code === code)
-      if (exists) {
+      if (colectionJSON.some((prod) => prod.code === code)) {
         console.log(`Ya existe un producto con el código ${code}`);
+        return;
+      }
+      if (id === null) {
+        while (colectionJSON.some((prod) => prod.id === this.#id)) {
+          this.#id = this.#id + 1;
+        }
+        id = this.#id;
       } else {
-        if(!id){
-          while (colectionJSON.some((prod) => prod.id === this.#id)) {
-            this.#id = this.#id + 1
-          }
-          product.id = this.#id;
-        } else {
-          if (colectionJSON.some((prod) => prod.id === id)) {
-            console.log(`Ya existe un producto con el id ${product.id}`)
-          } else {
-            product.status = true
-            if(!thumbnails || !Array.isArray(thumbnails)){
-              product.thumbnails = []
-            } 
-            colectionJSON.push({...product})
-            await fs.writeFile(this.path, JSON.stringify(colectionJSON))
-            console.log(`Se agregó el producto "${product.title}" a la colección`);
-          }
+        if (colectionJSON.some((prod) => prod.id === id)) {
+          console.log(`Ya existe un producto con el id ${id}`);
+          return;
         }
       }
+      let product = {
+        id: id,
+        title: title,
+        description: description,
+        price: price,
+        thumbnails: thumbnails,
+        code: code,
+        stock: stock,
+        category: category,
+        status: status,
+      };
+      colectionJSON.push({ ...product });
+      await fs.writeFile(this.path, JSON.stringify(colectionJSON));
+      console.log(`Se agregó el producto "${title}" a la colección`);
+    } catch (error) {
+      throw new Error(`Error al agregar producto: ${error.message}`);
     }
-    catch (error) {
-      throw new Error(`Error al agregar producto: ${error.message}`)
-    }
-  }
+  };
 
-  getProductById = async (productId) =>  {
+  getProductById = async (productId) => {
     try {
       let colectionJSON = await this.getProducts();
-      let exists = colectionJSON.find((prod) => prod.id === productId)
+      let exists = colectionJSON.find((prod) => prod.id === productId);
       if (exists) {
-        return exists
+        return exists;
       } else {
-        return `Not found id: ${productId}`
-      }
-    }
-    catch (error) {
-      throw new Error(`Error al encontrar el producto: ${error.message}`)
-    }
-  }
-  
-  updateProduct = async (productId, updates) => {
-    try {
-      let colectionJSON = await this.getProducts();
-      let exists = colectionJSON.find((prod) => prod.id === productId)
-      if (exists) {
-        let {title, description, price, thumbnails, code, stock, category,id,status} = updates
-        if (!id ||!title || !description || !price || !thumbnails || !code || !stock || !category || !status) {
-          console.log(`El producto debe tener todos los campos completos`);
-        } else {
-          await this.deleteProduct(productId)
-          await this.addProduct(updates)
-        }
-      } else {
-        return `Not found id: ${productId}`
+        return `Not found id: ${productId}`;
       }
     } catch (error) {
-      throw new Error(`Error al actualizar el producto: ${error.message}`)
+      throw new Error(`Error al encontrar el producto: ${error.message}`);
     }
-  }
+  };
+
+  updateProduct = async (productId, updates) => {
+    try {
+      let { id, title, description, price, thumbnails, code, stock, category, status } =
+        updates;
+      let colectionJSON = await this.getProducts();
+      let exists = colectionJSON.find((prod) => prod.id === productId);
+      if (exists) {
+        if (
+          !id ||
+          !title ||
+          !description ||
+          !price ||
+          !thumbnails ||
+          !code ||
+          !stock ||
+          !category ||
+          !status
+        ) {
+          console.log(`El producto debe tener todos los campos completos`);
+          return;
+        }
+        await this.deleteProduct(productId);
+        await this.addProduct(updates);
+      } else {
+        console.log(`Not found id: ${productId}`);
+      }
+    } catch (error) {
+      throw new Error(`Error al actualizar el producto: ${error.message}`);
+    }
+  };
 
   deleteProduct = async (productId) => {
     try {
       let colectionJSON = await this.getProducts();
       if (colectionJSON.find((prod) => prod.id === productId)) {
-        let newArray = colectionJSON.filter((prod) => prod.id !== productId)
-        await fs.writeFile(this.path, JSON.stringify(newArray))
+        let newArray = colectionJSON.filter((prod) => prod.id !== productId);
+        await fs.writeFile(this.path, JSON.stringify(newArray));
         console.log(`Se eliminó el producto con id: ${productId}:`);
         console.log(colectionJSON.find((prod) => prod.id === productId));
       } else {
         console.log(`No se encontró el producto con id: ${productId}`);
       }
+    } catch (error) {
+      throw new Error(`Error al eliminar producto: ${error.message}`);
     }
-    catch (error) {
-      throw new Error(`Error al eliminar producto: ${error.message}`)
-    }
-  }
+  };
 }
-module.exports =  ProductManager;
+module.exports = ProductManager;
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,51 +1,49 @@
-const express = require('express');
-const router = express.Router()
-const CartsManager = require('../cartsMaganer');
-const cm = new CartsManager('./carritos.json');
+const express = require("express");
+const router = express.Router();
+const CartsManager = require("../cartsMaganer");
+const cm = new CartsManager("./src/carritos.json");
 
-router.get('/api/carts', async (req,res ) => {
+router.get("/api/carts", async (req, res) => {
   try {
     const carts = await cm.getCarts();
     res.status(200).send(carts);
   } catch (error) {
-    res.status(500).send('Error al obtener productos');
+    res.status(500).send("Error al obtener productos");
   }
-})
+});
 
-router.get('/api/carts/:cid', async (req,res ) => {
-
+router.get("/api/carts/:cid", async (req, res) => {
   try {
     const cartId = parseInt(req.params.cid);
-    const cart =  await cm.getCartById(cartId);
+    const cart = await cm.getCartById(cartId);
     if (cart) {
       res.status(200).send(cart);
     } else {
-      res.status(404).send('Producto no encontrado');
+      res.status(404).send("Producto no encontrado");
     }
   } catch (error) {
-    res.status(500).send('Error al obtener el producto');
+    res.status(500).send("Error al obtener el producto");
   }
+});
 
-})
-
-router.post('/api/carts', (req,res) => {
-  try{
-    cm.addCart()
-    res.status(200).send('Se agregó correctamente el carrito')
+router.post("/api/carts", (req, res) => {
+  try {
+    cm.addCart();
+    res.status(200).send("Se agregó correctamente el carrito");
   } catch (error) {
-    res.status(500).send('Error al agregar el producto');
+    res.status(500).send("Error al agregar el producto");
   }
-})
+});
 
-router.post('/api/carts/:cid/product/:pid', async (req,res) => {
+router.post("/api/carts/:cid/product/:pid", async (req, res) => {
   try {
     const cartId = parseInt(req.params.cid);
     const productId = parseInt(req.params.pid);
-    cm.updateCart(cartId,productId)
-    res.status(200).send('Producto añadido al carrito')
+    cm.updateCart(cartId, productId);
+    res.status(200).send("Producto añadido al carrito");
   } catch (error) {
-    res.status(500).send('Error al cargar el producto');
+    res.status(500).send("Error al cargar el producto");
   }
-})
+});
 
 module.exports = router;
